d2a083c9140ca430e6acc494132cbd858a6cec4f
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-11-21 09:22:07 -0300

    Add Project

---
+++ index.js
@@ -0,0 +1,22 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+}
+
+const routes = {
+    "/": "./home.html"
+};
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path];
+    const html = await fetch(route).then((data) => data.text());
+    document.querySelector('#main').innerHTML = html; 
+}
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
